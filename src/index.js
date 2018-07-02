72d6ee8ce2bc6041715039fef846ededc1222ab9
Mark Hainline <wucheeman@gmail.com>
2018-07-02 15:43:12 -0400

    simplified Square to be a functional component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        // causes square to display 'x'
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square (props) {
+  return (
+    <button
+      className="square"
+      // causes square to display 'x'
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
